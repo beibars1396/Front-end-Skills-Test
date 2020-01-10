7463ff2cf8ee5de9f6cd54429114371a05631cf7
Beibars Ushbay <bei_bars@me.com>
2020-01-10 02:10:33 +0100

    BigButton impl

---
+++ src/components/BigButton.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.TouchableOpacity`
+    margin: 16px;
+    height: 50px;
+    width: 343;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.07);
+    border-radius: 60px;
+    backgroundColor: #28A745;
+`
+
+const Text = styled.Text`
+    
+    font-style: normal;
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 25px;
+
+    color: #FFFFFF;
+`
+
+// font-family: Open Sans;
+
+export default ({ onPress }) => (
+    <Container onPress={onPress} activeOpacity={0.75} >
+        <Text>Club Member</Text>
+    </Container>
+)
---
+++ src/components/Layout.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native'
+
+export const Layout = styled.View.attrs(props => ({
+
+}))`
+    flex: 1;
+    background-color: #E5E5E5;
+`
